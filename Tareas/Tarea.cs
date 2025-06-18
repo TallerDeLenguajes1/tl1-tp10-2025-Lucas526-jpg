a51681244db1aa8be91e3f8c4e91d1ba08071f37
Lucas526-jpg <Lucasfrx432@gmail.com>
2025-06-17 21:07:55 -0300

    ejercicio 2 terminado

---
+++ Tareas/Tarea.cs
@@ -0,0 +1,19 @@
+namespace EspacioTarea;
+public class Tarea{
+    public int userId
+    {
+        get;set;
+    }
+    public int id
+    {
+        get;set;
+    }
+    public string title
+    {
+        get;set;
+    }
+    public bool completed
+    {
+        get;set;
+    }
+}
