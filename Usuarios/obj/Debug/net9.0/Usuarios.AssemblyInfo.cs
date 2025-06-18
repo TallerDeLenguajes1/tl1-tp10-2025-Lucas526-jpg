ed29789082aed88195e6469710a919d179c16249
Lucas526-jpg <Lucasfrx432@gmail.com>
2025-06-17 21:06:27 -0300

    ejercicio 1 terminado

--- Usuarios/obj/Debug/net9.0/Usuarios.AssemblyInfo.cs
+++ Usuarios/obj/Debug/net9.0/Usuarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6f01fdfc5879bdc5de3a968ea410d4d96fef6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa448a11f366d94721ce30354dd09a27e063e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
